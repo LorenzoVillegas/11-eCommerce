3a58dc0692932294a8f205983967fcb890c1c91f
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2023-05-19 09:48:06 +0000

    funzionante

--- preverifica-Routing/src/app/app.component.ts
+++ preverifica-Routing/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ECommerce } from './models/ecommerce.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -8,17 +8,15 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   data: ECommerce;
   serviceURL: string = 'https://my-json-server.typicode.com/paolocarugati/PC_ecommerce/db';
   oECommerce: Observable<ECommerce>;
 
   constructor(public http: HttpClient) {
-    this.makeTypedRequest();
   }
 
-  makeTypedRequest() : void
-  {
+  ngOnInit() : void {
     
     this.oECommerce = this.http.get<ECommerce>(this.serviceURL);
     this.oECommerce.subscribe(d => {this.data = d;});
